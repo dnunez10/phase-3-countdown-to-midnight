0043b9d90e069c7a1b7a2fe25a1c98e1a82eee06
dnunez10 <diana_nunez10@yahoo.com>
2021-05-19 17:19:49 -0700

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown (x = 10)
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (x = 1)
+    sleep(x)
 end
